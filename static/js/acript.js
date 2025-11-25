7e372fb3b39acb68d8a0450e49f0862c5fecd592
zenshi99 <jorgitoquira15@gmail.com>
2025-11-25 10:17:30 -0500

    agregando commit

---
+++ static/js/acript.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Gracias por tu mensaje. Te responderé pronto.");
+});
